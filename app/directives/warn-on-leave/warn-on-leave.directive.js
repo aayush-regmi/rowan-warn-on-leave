73076f5257e21cb618dad1e980f543a61acaa9e4
rowansiwakoti <rowansiwakoti@gmail.com>
2018-03-15 11:10:54 +0545

    made changes according to review

--- app/directives/warn-on-leave/warn-on-leave.directive.js
+++ app/directives/warn-on-leave/warn-on-leave.directive.js
@@ -9,38 +9,40 @@
     function warnOnLeave($uibModal, $state, $window) {
         return {
 
-            require: '^form',
+            require: '?form',
 
             restrict: 'EA',
 
             link: function (scope, elem, attrs, form) {
 
-                var $locationChangeStartUnbind = scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+                if (form !== null) {
+                    var $locationChangeStartUnbind = scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
 
-                    if (form.$dirty && !form.$submitted) {
+                        if (form.$dirty && !form.$submitted) {
 
-                        event.preventDefault();
-
-                        var modalInstance = $uibModal.open({
-                            animation: true,
-                            templateUrl: '../app/directives/warn-on-leave/warn-on-leave-modal.html',
-                            controller: 'WarnOnLeaveController as warnOnLeaveCtrl',
-                            size: 'sm'
-                        });
-
-                        modalInstance.result.then(function (status) {
-                            form.$setPristine();
-                            $state.go(toState);
-                        }, function (status) {
                             event.preventDefault();
-                        });
-                    }
-                });
 
-                scope.$on('$destroy', function () {
-                    $window.onbeforeonload = null;
-                    $locationChangeStartUnbind();
-                });
+                            var modalInstance = $uibModal.open({
+                                animation: true,
+                                templateUrl: './directives/warn-on-leave/warn-on-leave-modal.html',
+                                controller: 'WarnOnLeaveController as warnOnLeaveCtrl',
+                                size: 'sm'
+                            });
+
+                            modalInstance.result.then(function () {
+                                form.$setPristine();
+                                $state.go(toState);
+                            }, function () {
+                                event.preventDefault();
+                            });
+                        }
+                    });
+
+                    scope.$on('$destroy', function () {
+                        $window.onbeforeonload = null;
+                        $locationChangeStartUnbind();
+                    });
+                }
             }
         };
     }
